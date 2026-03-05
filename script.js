dbef6869b38cea13d82f1de2ca277c36df4e0563
Daniel Alonso <danielalonsodias@gmail.com>
2026-03-05 14:16:54 -0300

    Atualização do portfolio

---
+++ script.js
@@ -0,0 +1,42 @@
+const text1 = "Daniel Alonso";
+const text2 = "Desenvolvedor Front-End";
+
+const element = document.getElementById("typing");
+
+let index = 0;
+let isDeleting = false;
+let currentText = text1;
+
+function typeEffect(){
+
+element.textContent = currentText.substring(0, index);
+
+if(!isDeleting){
+index++;
+
+if(index > currentText.length){
+
+setTimeout(()=>{
+isDeleting = true;
+},1000)
+
+}
+
+}else{
+
+index--;
+
+if(index === 0){
+
+isDeleting = false;
+currentText = text2;
+
+}
+
+}
+
+setTimeout(typeEffect,80);
+
+}
+
+typeEffect();
